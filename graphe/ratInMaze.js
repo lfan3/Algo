037bc8494fc04d6056178e41817f187b1f95d070
fll01148073 <fll01148073@alibaba-inc.com>
2023-04-28 00:28:02 +0800

    update

--- graphe/ratInMaze.js
+++ graphe/ratInMaze.js
@@ -1,12 +1,20 @@
 const rows = 4;
 const cols = 4;
+
 function ratMaze(grid, outGrid, r, c){
-  if(r>= rows && c>= cols && grid[r][c]===1){
+  if(r === rows-1 && c=== cols-1 && grid[r-1][c-1]===1){
     return outGrid
   }
   
 }
 
 function isFeasible(r, c){
-  return r<rows && c < cols
+  return r<rows && c < cols && r>=0 && c >=0 && grid[r][c]===1
 }
+
+const grid = [
+  [1,0,0,0]
+  [1,1,0,0]
+  [0,1,0,1]
+  [0,1,1,1]
+]
