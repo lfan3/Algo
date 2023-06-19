abdad97d7393f2132de38d118f24d6dfbbebd081
fll01148073 <fll01148073@alibaba-inc.com>
2023-06-19 15:40:51 +0800

    update from work

--- graphe/test.js
+++ graphe/test.js
@@ -1,8 +1,9 @@
-const loop = (g, k)=>{
-  const n = g[k];
-  for(let i=0; i<n; i++){
-    if(loop(g, ))
-  }
-}
 
 const graph = [[1],[0]]
+
+const m = [
+  {"a":12,"b":{"a":[12,13]}},
+  {"a":2,"b":14}
+]
+const c = [...m]
+console.log("c", c, c===m)
